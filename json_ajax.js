cb9c8af28d4b4088d4bf233b8997d501b0e7aba2
Vaughn Antonyan <vaughnanton@gmail.com>
2018-09-18 22:38:59 -0700

    JSON/AJAX initialize

---
+++ json_ajax.js
@@ -0,0 +1,16 @@
+//Similar to how user interfaces help people use programs, APIs help programs interact with other programs.
+
+/*
+AJAX
+-Programmers often use AJAX technologies when working with APIs.
+-AJAX originated form Asynchronous Javascript and XML
+-a group of technologies that make asynchronous requests to a server to transfer data, then load any data into page
+-an asynchronous process means the browser does not stop loading a page to wait for the server's response, also,
+the browser inserts updated data into part of the page wihtout having to refresh the entire page
+-UE benefits, page loads faster, requests/transfers happen in bg, when new data comes in only necessary part is updated
+
+JSON
+-the data transferred between the browser and server is often in a format called JavaScript Object Notation
+-JSON resembles JavaScript object literal syntax, except transferred as a string, once received it can be converted
+to an object and used in a script
+*/
